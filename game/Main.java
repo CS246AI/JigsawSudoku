ac165d6e6bf03b1c2af8af492f0bf14d73042624
surik-simyan <surik.simyan@gmail.com>
2024-05-08 10:59:41 +0400

    UI update

--- game/Main.java
+++ game/Main.java
@@ -8,6 +8,11 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
         Board board = new Board();
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
         SwingUtilities.invokeLater(() -> new MainFrame(board));
     }
 }
